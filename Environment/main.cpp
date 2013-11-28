33bd7d5c4a65281c8afb1150e73df02197be81bb
Big C <Big_C@smallC.com>
2013-11-28 20:36:48 +0100

    Init

---
+++ Environment/main.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.cpp
+ *
+ * Created on 28 listopad 2013, 20:28
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
---
+++ Robot/main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.cpp
+ * Created on 28 listopad 2013, 20:29
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
